1b9ec9866a0aa504c4aa76da8da3bb5772e26310
tee-moore <2teemoore@gmail.com>
2018-07-20 11:08:25 +0300

    Init: change config

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+1.git clone https://github.com/tee-moore/todo.git
+2.composer update
+3.php init ->  Development
+4.create database todo
+5.php yii migrate
+
+
+
+
 <p align="center">
     <a href="https://github.com/yiisoft" target="_blank">
         <img src="https://avatars0.githubusercontent.com/u/993323" height="100px">
--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=todo',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
     ],
 ];
